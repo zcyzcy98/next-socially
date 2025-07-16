9ac172fad1666c82999ce01c9a5b60f2d754d43c
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2025-07-16 19:14:32 +0800

    update: meta数据

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,10 +1,25 @@
+import { getProfileByUsername } from "@/actions/profile.action";
 import React from "react";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
+}
 
 export default function ProfilePage({
   params,
 }: {
   params: { username: string };
 }) {
-  console.log(params)
+  console.log(params);
   return <div>ProfilePage</div>;
 }
